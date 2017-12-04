b5443a3439fde61b2a1aa06b0137d7049efd9851
Matt Warman <warmanmr@gmail.com>
2017-12-04 09:07:59 -0500

    Organized source files for routes and components.

--- src/app.js
+++ src/app.js
@@ -1,58 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Link, NavLink, Switch, Route } from 'react-router-dom';
 
-const ExpenseDashboardPage = () => (
-  <div>Welcome to the Dashboard</div>
-);
-
-const AddExpensePage = () => (
-  <div>Add an Expense</div>
-);
-
-const EditExpensePage = () => (
-  <div>Edit an Expense</div>
-);
-
-const HelpPage = () => (
-  <div>Get Help</div>
-);
-
-const Header = () => (
-  <header>
-    <h1>Expensify</h1>
-    <nav>
-      <ul>
-        <li><NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink></li>
-        <li><NavLink to="/create" activeClassName="is-active">Add Expense</NavLink></li>
-        <li><NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink></li>
-        <li><NavLink to="/help" activeClassName="is-active">Help</NavLink></li>
-      </ul>
-    </nav>
-  </header>
-);
-
-const NotFoundPage = () => (
-  <div>404 - <Link to="/">Go Home</Link></div>
-);
+import AppRouter from './routers/AppRouter';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-const routes = (
-  <BrowserRouter>
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={ExpenseDashboardPage} />
-        <Route exact path="/" component={ExpenseDashboardPage} />
-        <Route path="/create" component={AddExpensePage} />
-        <Route path="/edit" component={EditExpensePage} />
-        <Route path="/help" component={HelpPage} />
-        <Route component={NotFoundPage} />
-      </Switch>
-    </div>
-  </BrowserRouter>
-);
-
-ReactDOM.render(routes, document.getElementById('app'));
+ReactDOM.render(<AppRouter />, document.getElementById('app'));
---
+++ src/components/EditExpensePage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const EditExpensePage = () => (
+  <div>Edit an Expense</div>
+);
+
+export default EditExpensePage;
