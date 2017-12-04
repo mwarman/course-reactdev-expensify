23382e6e8ea39f688d0ea5e3510d49121d1ecfd7
Matt Warman <warmanmr@gmail.com>
2017-12-04 09:18:40 -0500

    Route properties.

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
-const EditExpensePage = () => (
-  <div>Edit an Expense</div>
-);
+const EditExpensePage = (props) => {
+  console.log('props', props);
+  return (
+    <div>Editing expense {props.match.params.id}</div>
+  );
+}
 
 export default EditExpensePage;
