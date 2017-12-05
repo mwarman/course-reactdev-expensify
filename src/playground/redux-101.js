61cacf9a99c8a09926ffeb50bd5bbfa83740f3d9
Matt Warman <warmanmr@gmail.com>
2017-12-05 07:42:56 -0500

    Redux action generators.

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,18 +1,34 @@
 import { createStore } from 'redux';
 
+// Action Generators - Functions that return action objects
+
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
+  type: 'INCREMENT',
+  incrementBy
+});
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+  type: 'DECREMENT',
+  decrementBy
+});
+const setCount = ({ count } = {}) => ({
+  type: 'SET',
+  count
+});
+const resetCount = () => ({
+  type: 'RESET'
+});
+
 // Redux Store - a container for application state
 
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case 'INCREMENT':
-      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
       return {
-        count: state.count + incrementBy
+        count: state.count + action.incrementBy
       };
     case 'DECREMENT':
-      const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
       return {
-        count: state.count - decrementBy
+        count: state.count - action.decrementBy
       };
     case 'SET':
       return {
@@ -34,30 +50,15 @@ const unsubscribe = store.subscribe(() => {
 // Redux Action - an object that is sent to the store
 
 // Increment the count state
-store.dispatch({
-  type: 'INCREMENT',
-  incrementBy: 5
-});
-store.dispatch({
-  type: 'INCREMENT'
-});
+store.dispatch(incrementCount({ incrementBy: 5 }));
+store.dispatch(incrementCount());
 
 // Reset the count state
-store.dispatch({
-  type: 'RESET'
-});
+store.dispatch(resetCount());
 
 // Decrement the count state
-store.dispatch({
-  type: 'DECREMENT',
-  decrementBy: 5
-});
-store.dispatch({
-  type: 'DECREMENT'
-});
+store.dispatch(decrementCount({ decrementBy: 5 }));
+store.dispatch(decrementCount());
 
 // Explicitly set the count state
-store.dispatch({
-  type: 'SET',
-  count: 101
-})
+store.dispatch(setCount({ count: 101 }));
