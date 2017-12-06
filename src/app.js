e7d41d41613df4a6c2b0a218ec384924f552847b
Matt Warman <warmanmr@gmail.com>
2017-12-06 06:53:15 -0500

    Moved Redux logic into Expensify app components and source files.

--- src/app.js
+++ src/app.js
@@ -2,8 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+
+const store = configureStore();
+
+store.dispatch(addExpense({ description: 'Water Bill', amount: 17500 }));
+store.dispatch(addExpense({ description: 'Gas Bill', amount: 8600 }));
+store.dispatch(setTextFilter('water'));
+
+const state = store.getState();
+console.log('state:', state);
+console.log(getVisibleExpenses(state.expenses, state.filters));
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
