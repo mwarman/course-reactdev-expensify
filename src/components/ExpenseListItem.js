8716bfb16515ae806f642cca22a686dfdff043ab
Matt Warman <warmanmr@gmail.com>
2017-12-09 08:13:48 -0500

    Snapshot testing with dynamic components.

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
+export const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
   <div>
     <h3><Link to={`/edit/${id}`}>{description}</Link></h3>
     <p>
