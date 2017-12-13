61635d14bfbf50ca51b719c5d37c6ac1a80d55f0
Matt Warman <warmanmr@gmail.com>
2017-12-13 09:27:11 -0500

    Playground - Firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,4 +1,5 @@
 import * as firebase from 'firebase';
+import moment from 'moment';
 
 const config = {
   apiKey: "AIzaSyDj5nF3zsCWrsIFeDLxuLJQ5TnmymKlCbQ",
@@ -13,30 +14,171 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-database.ref().set({
-  name: 'Matthew Warman',
-  age: 39,
-  isSingle: false,
-  location: {
-    city: 'Raleigh',
-    country: 'United States'
-  }
-}).then(() => {
-  console.log('Data is saved!');
-}).catch((err) => {
-  console.log('Firebase error:', err);
-});
+// Push Expenses onto the DB
+// const expenses = [{
+//   description: 'Gum',
+//   note: 'Bad breath',
+//   amount: 195,
+//   createdAt: moment().valueOf()
+// }, {
+//   description: 'Rent',
+//   note: 'Gotta live somewhere',
+//   amount: 109500,
+//   createdAt: moment().subtract(4, 'days').valueOf()
+// }, {
+//   description: 'Credit Card',
+//   note: 'Healthy credit score is a priority',
+//   amount: 4500,
+//   createdAt: moment().add(4, 'days').valueOf()
+// }];
+//
+// expenses.forEach((expense) => {
+//   database.ref('expenses').push(expense);
+// });
 
-// database.ref().set('This is my data.');
+// const snapshotToArray = (snapshot) => {
+//   const expenses = [];
+//   snapshot.forEach((childSnapshot) => {
+//     expenses.push({
+//       id: childSnapshot.key,
+//       ...childSnapshot.val()
+//     });
+//   });
+//   return expenses;
+// };
 
-// database.ref('age').set(33);
-// database.ref('location/city').set('Wilmington');
+// database.ref('expenses')
+//   .once('value')
+//   .then((snapshot) => {
+//     const expenses = snapshotToArray(snapshot);
+//     console.log('expenses', expenses);
+//   }).catch((err) => {
+//     console.log('Fetch once failed.', err);
+//   });
 
-database.ref('attributes').set({
-  height: 76,
-  weight: 175
-}).then(() => {
-  console.log('Attributes are set!');
-}).catch((err) => {
-  console.log('Firebase error:', err);
-});
+// Event: value -- all value changes
+// database.ref('expenses')
+//   .on('value', (snapshot) => {
+//     const expenses = snapshotToArray(snapshot);
+//     console.log('expenses', expenses);
+//   }, (err) => {
+//     console.log('failed to process changed snapshot', err);
+//   });
+
+// Event: child_removed -- expense deleted
+// database.ref('expenses')
+//   .on('child_removed', (snapshot) => {
+//     console.log(`child_removed key:${snapshot.key}, value:${JSON.stringify(snapshot.val())}`);
+//   }, (err) => {
+//     console.log('failed to process changed snapshot', err);
+//   });
+
+// Event: child_changed -- expense updated
+// database.ref('expenses')
+//   .on('child_changed', (snapshot) => {
+//     console.log(`child_changed key:${snapshot.key}, value:${JSON.stringify(snapshot.val())}`);
+//   }, (err) => {
+//     console.log('failed to process changed snapshot', err);
+//   });
+
+// Event: child_added -- new expense inserted
+// database.ref('expenses')
+//   .on('child_added', (snapshot) => {
+//     console.log(`child_added key:${snapshot.key}, value:${JSON.stringify(snapshot.val())}`);
+//   }, (err) => {
+//     console.log('failed to process changed snapshot', err);
+//   });
+
+// Push an Object onto an Array (collection)
+// database.ref('notes').push({
+//   title: 'Course Topics',
+//   body: 'React Native, Angular, Python'
+// });
+
+// Set data (on the root)
+// database.ref()
+//   .set({
+//     name: 'Matthew Warman',
+//     age: 39,
+//     stressLevel: 6,
+//     job: {
+//       title: 'Software Developer',
+//       company: 'Google'
+//     },
+//     location: {
+//       city: 'Raleigh',
+//       country: 'United States'
+//     }
+//   }).then(() => {
+//     console.log('Set succeeded.');
+//   }).catch((err) => {
+//     console.log('Set failed.', err);
+//   });
+
+// Fetch data once
+// database.ref()
+//   .once('value')
+//   .then((data) => {
+//     const val = data.val();
+//     console.log('Fetch succeeded.', val);
+//   }).catch((err) => {
+//     console.log('Fetch failed.', err);
+//   });
+
+// Fetch data when data change events occur
+// const onValueChange = database.ref()
+//   .on('value', (data) => {
+//     console.log('Fetch succeeded.', data.val());
+//   }, (err) => {
+//     console.log('Fetch failed.', err);
+//   });
+//
+// setTimeout(() => {
+//   database.ref('age').set(55);
+// }, 3000);
+//
+// setTimeout(() => {
+//   database.ref().off('value', onValueChange);
+// }, 6000);
+//
+// setTimeout(() => {
+//   database.ref('age').set(30);
+// }, 9000);
+
+// Challenge - Subscribe to Data Value Changes
+// const onValueChange = (snapshot) => {
+//   const data = snapshot.val();
+//   console.log(`${data.name} is a ${data.job.title} at ${data.job.company}.`);
+// };
+// database.ref()
+//   .on('value', onValueChange, (err) => {
+//     console.log('Problem subscribing to value changes.', err);
+//   });
+// setTimeout(() => {
+//   database.ref('name').set('Matt Warman');
+// }, 3000);
+// setTimeout(() => {
+//   database.ref('job/title').set('Engineer');
+// }, 6000);
+// setTimeout(() => {
+//   database.ref('job/company').set('Amazon');
+// }, 9000);
+
+// database.ref()
+//   .update({
+//     stressLevel: 9,
+//     'job/company': 'Amazon',
+//     'location/city': 'Seattle'
+//   }).then(() => {
+//     console.log('Update succeeded.');
+//   }).catch((err) => {
+//     console.log('Update failed.', err);
+//   });
+
+// database.ref()
+//   .remove()
+//   .then(() => {
+//     console.log('Remove succeeded.');
+//   }).catch((err) => {
+//     console.log('Remove failed.', err);
+//   });
