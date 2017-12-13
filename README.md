1256b015e4aba518dc021524e880607f9088e693
Matt Warman <warmanmr@gmail.com>
2017-12-13 06:15:43 -0500

    Installed and configured Firebase.

--- README.md
+++ README.md
@@ -26,6 +26,7 @@ This repository contains work associated with the Udemy course [The Complete Rea
 **Production**
 
 [Express](http://expressjs.com/)  
+[Firebase](https://firebase.google.com)  
 [React](https://reactjs.org/docs)  
 [React Modal](https://reactcommunity.org/react-modal/)  
 [React Router](https://reacttraining.com/react-router/web/guides/philosophy)  
---
+++ src/firebase/firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyDj5nF3zsCWrsIFeDLxuLJQ5TnmymKlCbQ",
+  authDomain: "expensify-f9314.firebaseapp.com",
+  databaseURL: "https://expensify-f9314.firebaseio.com",
+  projectId: "expensify-f9314",
+  storageBucket: "expensify-f9314.appspot.com",
+  messagingSenderId: "276932573557"
+};
+
+firebase.initializeApp(config);
+
+firebase.database().ref().set({
+  name: 'Matthew Warman'
+});
