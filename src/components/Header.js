46f5697f11ec1e76d721db68c40d2c9006da62c8
Matt Warman <warmanmr@gmail.com>
2017-12-15 08:06:00 -0500

    Added logout feature.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const Header = () => (
+import { startLogout } from '../actions/auth';
+
+export const Header = (props) => (
   <header>
     <h1>Expensify</h1>
     <nav>
@@ -10,8 +13,17 @@ const Header = () => (
         <li><NavLink to="/create" activeClassName="is-active">Add Expense</NavLink></li>
         <li><NavLink to="/help" activeClassName="is-active">Help</NavLink></li>
       </ul>
+      <button
+        onClick={props.startLogout}
+      >
+        Logout
+      </button>
     </nav>
   </header>
 );
 
-export default Header;
+const mapDispatchToProps = (dispatch) =>  ({
+  startLogout: () => dispatch(startLogout())
+});
+
+export default connect(undefined, mapDispatchToProps)(Header);
